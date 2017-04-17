9ccbea76567203474d406ec2c79a24a3999d3dbc
xiweeya <xiweeya@users.noreply.github.com>
2017-04-17 17:36:09 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,120 @@
-var gulp = require('gulp');//获取 gulp
-var util = require('gulp-util');//输出带颜色的提示信息
-var autoprefixer = require('gulp-autoprefixer');//自动补全浏览器前缀
-var uglify = require('gulp-uglify');//用于压缩js的模块
-var watchpath = require('gulp-watch-path');//检测改变的js文件
-var watch = require('gulp-watch');//只编译被更改过的文件
-var sourcemaps = require('gulp-sourcemaps');//帮助压缩过的js调试
-var sass = require('gulp-ruby-sass');//用于编译sass的模块
-var cleancss = require('gulp-clean-css');//用于压缩css的模块
-var imagemin = require('gulp-imagemin');//用于压缩图片的模块
-var rename = require('gulp-rename');//重命名 添加.min后缀
+var gulp = require('gulp');
+var util = require('gulp-util');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var cleancss = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
+var watchpath = require('gulp-watch-path');
+var watch = require('gulp-watch');
+// var rev = require('gulp-rev');
+// var recCollector = require('gulp-rev-collector');
 
-var babel = require('gulp-babel');//ES6
-//路径
-var jsSrc = 'js/', jsDest = 'dist/js/',
-	cssSrc = 'sass/', cssDest = 'dist/css/',
-	imgSrc = 'img/', imgDest = 'img/';
+var jsSrc = 'source/script/', jsDest = 'dist/script/',
+    cssSrc = 'source/style/', cssDest = 'dist/style/',
+    imgSrc = 'image/', imgDest = 'image/',
+    htmlSrc = 'source/pages/', htmlDest = '';
 
-//压缩所有的js文件
-gulp.task('alljs',function(){
-	gulp.src(jsSrc+'*.js')
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest(jsDest))
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest(jsDest))
+gulp.task('js',function(){
+    gulp.src([jsSrc+'*.js','!'+jsSrc+'*.min.js'])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest(jsDest))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(jsDest))
 });
-//检测到某个js文件被修改时，只编写当前修改的js文件
-gulp.task('watchjs',function(){
-	gulp.watch(jsSrc+'*.js', function(event){
-		var paths = watchpath(event, jsSrc, jsDest);
-		util.log(util.colors.green(event.type)+' '+paths.srcPath);
-		gulp.src(paths.srcPath)
-			.pipe(babel({
-				presets: ['es2015']
-			}))
-			.pipe(gulp.dest(jsDest))
-			.pipe(uglify())
-			.pipe(rename({
-				suffix: '.min'
-			}))
-			.pipe(gulp.dest(paths.distDir));
-		util.log('Dist '+paths.distPath);
-	})
+gulp.task('watchjs', function () {
+    gulp.watch([jsSrc+'*.js','!'+jsSrc+'*.min.js'], function (event) {
+        var paths = watchpath(event, jsSrc, jsDest);
+        util.log(util.colors.green(event.type) + ' ' + paths.srcPath);
+        gulp.src(paths.srcPath)
+            .pipe(babel({
+                presets: ['es2015']
+            }))
+            .pipe(gulp.dest(jsDest))
+            .pipe(uglify())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest(paths.distDir));
+        util.log('Dist ' + paths.distPath);
+    })
 });
-
-//编译所有的sass文件
-gulp.task('allsass',function(){
-	sass([cssSrc+'*.scss',cssSrc+'*.sass'])
-		.on('error',function(err){
-			console.error('Error:', err.message);
-		})
-		.pipe(autoprefixer({
-			browsers: 'last 2 versions'
-		}))
-		.pipe(gulp.dest(cssDest))
-		.pipe(cleancss())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest(cssDest));
+gulp.task('css',function(){
+    gulp.src([cssSrc+'*.css','!'+cssSrc+'*.min.css'])
+        .on('error',function(err){
+            console.error('Error:', err.message);
+        })
+        .pipe(autoprefixer({
+            browsers: 'last 2 versions'
+        }))
+        .pipe(cleancss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        // .pipe(rev())
+        .pipe(gulp.dest(cssDest));
 });
-//编译修改的sass文件
-gulp.task('watchsass', function(){
-	gulp.watch(cssSrc+'*.scss', function(event){
-		var paths = watchpath(event, cssSrc, cssDest);
-		util.log(util.colors.green(event.type)+' '+paths.srcPath);
-		sass(paths.srcPath)
-			.on('error',function(err){
-				console.error('Error:', err.message);
-			})
-			.pipe(autoprefixer({
-				browsers: 'last 2 versions'
-			}))
-			.pipe(gulp.dest(cssDest))
-			.pipe(cleancss())
-			.pipe(rename({
-				suffix: '.min'
-			}))
-			.pipe(gulp.dest(paths.distDir));
-		util.log('Dist '+paths.distPath);
-	})
+gulp.task('sass',function(){
+    gulp.src([cssSrc+'*.scss',cssSrc+'*.sass'])
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: 'last 2 versions'
+        }))
+        .pipe(gulp.dest(cssDest))
+        .pipe(cleancss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        // .pipe(rev())
+        .pipe(gulp.dest(cssDest));
 });
-
-//压缩图片
+gulp.task('watchsass',function () {
+    gulp.watch([cssSrc+'*.scss',cssSrc+'*.sass'], function (event) {
+        var paths = watchpath(event, cssSrc, cssDest);
+        util.log(util.colors.green(event.type) + ' ' + paths.srcPath);
+        gulp.src(paths.srcPath)
+            .pipe(sass.sync().on('error', sass.logError))
+            .pipe(autoprefixer({
+                browsers: 'last 2 versions'
+            }))
+            .pipe(gulp.dest(cssDest))
+            .pipe(cleancss())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest(paths.distDir));
+        util.log('Dist ' + paths.distPath);
+    })
+});
 gulp.task('image', function(){
-	gulp.src(imgSrc+'*')
-		.pipe(imagemin({
-			propressive: true
-		}))
-		.pipe(gulp.dest('img'));
+    gulp.src([imgSrc+'*.jpg',imgSrc+'*.png'])
+        .pipe(imagemin({
+            propressive: true
+        }))
+        .pipe(gulp.dest(imgDest));
 });
-// gulp.task('watchimage', function(){
-// 	gulp.watch(imgSrc+'*', function(event){
-// 		var paths = watchpath(event, imgSrc, imgDest);
-//
-// 		util.log(util.colors.green(event.type)+' '+paths.srcPath);
-// 		util.log('Dist '+paths.distPath);
-//
-// 		glup.src(paths.srcPath)
-// 		    .pipe(imagemin({
-// 		    	propressive: true
-// 		    }))
-// 		    .pipe(gulp.dest(paths.distDir));
-// 	})
-// })
+gulp.task('html', function () {
+    var options = {
+        removeComments: true,
+        collapseWhitespace: true,
+        // collapseBooleanAttributes: true,
+        // removeEmptyAttributes: true,
+        // removeScriptTypeAttributes: true,
+        // removeStyleLinkTypeAttributes: true,
+        minifyJS: true,
+        minifyCSS: true
+    };
+    gulp.src([htmlSrc+'*.html',htmlSrc+'*.htm'])
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest(htmlDest));
+});
 
-gulp.task('default', ['watchjs','watchsass']);
-
+// gulp.task('default', ['alljs','allcss','image']);
